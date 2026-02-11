844e5e61b9a339089be6462eb22a17b4b2829207
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-02-10 22:03:25 -0500

    feat: Add a findMaxNumber second version with non-tail recursion

--- src/main/java/co/edu/uniquindio/recursion/MaxNumber.java
+++ src/main/java/co/edu/uniquindio/recursion/MaxNumber.java
@@ -19,7 +19,19 @@ public class MaxNumber {
         }
     }
 
+    public static int findMaxNumber2(int[] arr){
+        return  findMaxNumber2(arr, 0);
+    }
+
+    private static int findMaxNumber2(int[] arr, int index){
+        if (index == arr.length - 1){
+            return arr[index];
+        }
+        int maxExcedent = findMaxNumber2(arr, index+1);
+        return arr[index] > maxExcedent?arr[index]:maxExcedent;
+    }
+
     public static void main(String[] args) {
-        System.out.println(findMaxNumber(new int[]{4,1,6,10,2,0,18,3}));
+        System.out.println(findMaxNumber(new int[]{4,1,6,10,2,0,18,3, 21, 22, 80,1,0,2, 100}));
     }
 }
